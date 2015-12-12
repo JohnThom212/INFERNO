9869db3beb587361459bb4228e92eb5c36de4330
mulcairk <mulcairk@colorado.edu>
2015-12-12 15:04:09 -0700

    Updating case stress code with comments

--- Models/Imaging Stress Model/MountStress.m
+++ Models/Imaging Stress Model/MountStress.m
@@ -1,20 +1,25 @@
+%% INFERNO Camera Mount Swivel Stress Model
 clear all; close all; clc;
 
-Py = 30e6;
+%% Material Properties
+    sigmamax = 30e6; %tension yield stress of ABS P430, Pa
 
-L = 32e-3;
-m = 0.124;
-g = 9.81;
+%% Swivel Properties
+    r = 2.5e-3; %inner radius, m
+    R = 5e-3; %outer radius, m
+    k = R/sqrt(2); %average radius of friction, m
+    A = 4*pi*(R^2-r^2); %frictional surface area, m2
+    mu = 0.08; %minimum coeff of static friction for ABS
 
-r = 2.5e-3;
-R = 5e-3;
-k = R/sqrt(2);
-A = 4*pi*(R^2-r^2);
-mu = 0.08;
+%% Generate Moment from Case
+    L = 32e-3; %length of moment arm, m
+    m = 0.124; %mass of case/camera, kg
+    g = 9.81; %gravitational acceleration, m/s2
+    M = 2*m*g*L; %moment from 2g load, N-m
 
-M = 2*m*g*L;
-ps = M/A/k;
-Fs = ps*A;
-Fn = Fs/mu;
-pn = Fn/A;
-FOS = 30e6/pn;
+%% Determine Stress
+    sigmas = M/A/k; %shear stress to counteract moment, Pa
+    Fs = sigmas*A; %net shear force, N
+    Fn = Fs/mu; %net normal force required to generate shear force, N
+    pn = Fn/A; %normal stress, N
+    FOS = sigmamax/pn; %factor of safety
